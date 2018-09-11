8c8e7eb770db64ee9450f31f4176bb3a423f95d5
alejandroclose@gmail.com <alejandroclose@gmail.com>
2018-09-11 03:46:38 +0200

    fixed scroll navbar text color

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -3,5 +3,8 @@ console.log('main.js');
 
 $(window).scroll(function(){
   $('.navbar').toggleClass('scrolled', $(this).scrollTop()>400);
+  $('.navbar').toggleClass('navbar-dark', $(this).scrollTop()<400);
+  $('.navbar').toggleClass('navbar-light', $(this).scrollTop()>400);
   $('#logo').toggleClass('navbar-brand', $(this).scrollTop()>400);
+  
 });
