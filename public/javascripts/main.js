e704e9f6b83b0d07fa2712ec5866fef3c8c0d86d
alejandroclose@gmail.com <alejandroclose@gmail.com>
2018-09-12 00:50:04 +0200

    added cover logo and texts

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -2,9 +2,9 @@ console.log('main.js');
 // Change navbar color on scroll
 
 $(window).scroll(function(){
-  $('.navbar').toggleClass('scrolled', $(this).scrollTop()>400);
-  $('.navbar').toggleClass('navbar-dark', $(this).scrollTop()<400);
-  $('.navbar').toggleClass('navbar-light', $(this).scrollTop()>400);
-  $('#logo').toggleClass('navbar-brand', $(this).scrollTop()>400);
+  $('.navbar').toggleClass('scrolled', $(this).scrollTop()>300);
+  $('.navbar').toggleClass('navbar-dark', $(this).scrollTop()<300);
+  $('.navbar').toggleClass('navbar-light', $(this).scrollTop()>300);
+  $('#nav-logo').toggleClass('navbar-brand', $(this).scrollTop()>300);
   
 });
