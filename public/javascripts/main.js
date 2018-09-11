76f1efc12710fbe870f32e37d5af9d753e78d29f
alejandroclose@gmail.com <alejandroclose@gmail.com>
2018-09-11 02:48:35 +0200

    added cover photo

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -2,6 +2,6 @@ console.log('main.js');
 // Change navbar color on scroll
 
 $(window).scroll(function(){
-  $('.navbar').toggleClass('scrolled', $(this).scrollTop()>800);
-  $('#logo').toggleClass('navbar-brand', $(this).scrollTop()>800);
+  $('.navbar').toggleClass('scrolled', $(this).scrollTop()>400);
+  $('#logo').toggleClass('navbar-brand', $(this).scrollTop()>400);
 });
